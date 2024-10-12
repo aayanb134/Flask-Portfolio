ce65da9e50ad21c32bf0b22d731e68d8ba22fccd
aayanb134 <aayanb134@gmail.com>
2024-10-12 13:31:09 +0100

    vpc and security group

--- IaC/variables.tf
+++ IaC/variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-2"
 }
+
+variable "cidr_block" {
+  description = "cidr block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "app_name" {
+  description = "application name"
+  type        = string
+  default     = "portfolio"
+}
---
+++ IaC/vpc.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.app_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.app_name}-public-subnet-${count.index}"
+  }
+}
